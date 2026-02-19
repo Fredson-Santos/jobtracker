7ab0541fa7d3c7e949122047746167c8d006295d
Fred <fredsonf219@gmail.com>
2026-02-19 01:43:41 -0300

    fix(frontend): define porta padrao como 8016 no vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,10 +4,17 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173,
+    port: 8016,
+    strictPort: true,
+    host: true,
     proxy: {
       '/vagas': 'http://localhost:8015',
       '/alertas': 'http://localhost:8015',
     },
   },
+  preview: {
+    port: 8016,
+    strictPort: true,
+    host: true,
+  }
 })
